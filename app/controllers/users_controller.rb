41432872cd2850da5b18101509442fcd62bd5797
Michael Minter <mothore@gmail.com>
2019-05-29 08:57:04 -0700

    Caching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,18 @@
 class UsersController < ApplicationController
-  # before_action :set_timeline, only: :index
-  # before_action :set_user
-
   def index
-    # Rails.cache.fetch("timeline-#{params[:id] || 'casetabs'}", expires_in: 5.minutes) do
-    #   MicroBloggingService.new.list(params[:id] || 'casetabs', params[:count] || 20)
-    # end
-    @timeline = JSON.parse(File.read('spec/fixtures/timeline.json')) #, object_class: ::TimelineSerializer)
+    Rails.cache.fetch("timeline-#{params[:id] || 'casetabs'}", expires_in: 5.minutes) do
+      @timeline = JSON.parse(File.read('spec/fixtures/timeline.json'))
+    end
+    fresh_when(@timeline)
   end
 
   def show
-    # Rails.cache.fetch("user-#{params[:id] || 'casetabs'}", expires_in: 5.minutes) do
-    #   MicroBloggingService.new.user(params[:id] || 'casetabs')
-    # end
-    @user = JSON.parse(File.read('spec/fixtures/user.json'))
-    render json: @user
-  end
+    Rails.cache.fetch("user-#{params[:id] || 'casetabs'}", expires_in: 5.minutes) do
+      @user = JSON.parse(File.read('spec/fixtures/user.json'))
+    end
 
-  private
-
-  def set_timeline
-    # @timeline = Rails.cache.fetch("timeline-#{params[:id] || 'casetabs'}", expires_in: 5.minutes)
-  end
-
-  def set_user
-    # @user = Rails.cache.fetch("user-#{params[:id] || 'casetabs'}", expires_in: 5.minutes)
+    if stale?(@user)
+      render json: @user
+    end
   end
 end
